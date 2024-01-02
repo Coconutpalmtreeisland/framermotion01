102270e22cb9dcd5dbb9f00a253b2e1b35f5ba5e
coconutpalmtreeisland <adorableness1215@naver.com>
2024-01-02 14:15:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Project from './components/project/Project'
+import Modal from './components/modal/Modal'
 
-function App() {
+const App = () => {
+
+  const texts = [
+    {
+      title: "Image Quiz site",
+      subTitle: "project1",
+      src: "project2.png",
+      date: "2023",
+    },
+    {
+      title: "React Youtube site",
+      subTitle: "project2",
+      src: "project1.png",
+      date: "2023",
+    },
+    {
+      title: "Sexy Brain site",
+      subTitle: "project3",
+      src: "project2.png",
+      date: "2023",
+    },
+    {
+      title: "Cllo site",
+      subTitle: "project1",
+      src: "project1.png",
+      date: "2023",
+    },
+  ]
+
+  const [modal, setModal] = useState({ active: false, index: 0 })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <main className='main'>
+      <section id='section1'>
+        <h1>Gallery Hover Animation</h1>
+      </section>
+      <section id='section2' className='contents'>
+        <div className='project__wrap'>
+          {texts.map((text, index) => {
+            return <Project key={index} index={index} text={text} setModal={setModal} />
+          })}
+        </div>
+        <Modal modal={modal} texts={texts} />
+      </section>
+      <section id='section3'></section>
+    </main>
+  )
 }
 
-export default App;
+export default App
